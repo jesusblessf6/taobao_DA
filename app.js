1e828f2e93725f5d7e78a611c9fc9914f23ba42c
wing <jesusblessf6@gmail.com>
2013-12-23 11:23:08 +0800

    update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
+//var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
@@ -30,8 +30,8 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-var myRouter = require('./routes/myRouter');
-myRouter(app);
+//var myRouter = require('./routes/myRouter');
+//myRouter(app);
 
 //init the web socket
 var server = http.createServer(app);
@@ -41,7 +41,7 @@ server.listen(app.get('port'), function(){
 });
 
 //handle the socket event
-var listener = require('./routes/socketListener');
-listener(io);
+//var listener = require('./routes/socketListener');
+//listener(io);
 
 
