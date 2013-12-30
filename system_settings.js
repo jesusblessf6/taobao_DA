6202d4caacea9c28637134270a0335720c280778
wing <jesusblessf6@gmail.com>
2013-12-30 17:29:00 +0800

    update

--- system_settings.js
+++ system_settings.js
@@ -2,8 +2,11 @@
 	the configuration info
 */
 
+var crawlerTypes = ["global", "global trades", "brand meta", "product meta", "sku meta", "item meta", "shop meta", "specific brand", "specific shop"];
+
 var server = "localhost";
 var port = "3001"
 
 exports.server = server;
 exports.port = port;
+exports.crawlerTypes = crawlerTypes;
