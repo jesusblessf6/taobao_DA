662cdf6a5d044c681f2e0a4b3fa7c8c00a8a6ee9
wing <jesusblessf6@gmail.com>
2014-01-03 17:46:03 +0800

    update

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var mom = require('moment');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
 app.set('views', path.join(__dirname, 'views'));
 app.set('process_token', mom().zone(0).format('X'));
 
